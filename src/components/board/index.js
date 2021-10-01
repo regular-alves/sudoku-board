7796ac813b38b8f99a662d6a48d17afe5328537b
Rodrigo Alves <rodrigo.gois@live.com>
2021-09-30 21:27:08 -0300

    change: field as object

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -3,31 +3,84 @@ import './style.css';
 import Row from '../row';
 
 const Board = ({length}) => {
+    const section = Math.sqrt(length);
+    const [active, setActive] = useState([0,0]);
     const [fields, setFields] = useState([
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
+        [null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null],
+        [null,null,null,null,null,null,null,null,null],
     ]);
 
-    const setField = (y,x,value) => {
+    const setField = (y,x,v) => {
         let newFields = fields.slice();
         
         if(newFields[y][x]!==undefined) {
-            newFields[y][x] = value
+            newFields[y][x] = {
+                value: v,
+                error: false
+            }
         }
 
-        setFields(newFields);
+        getSection(y, x);
+
+        let nextY = y;
+        let nextX = x + 1;
+        
+        if(nextX>=length) {
+            nextY = nextY + 1;
+            nextX = 0;
+        }
+
+        if(nextY>=length) {
+            nextY = 0;
+        }
+
+        setActive([nextY, nextX]);
     }
 
-    useEffect(() => {
-        console.table(fields);
-    }, [fields]);
+    const getRow = (y) => {
+        let resultSet = fields[y] || [];
+
+        return resultSet.filter(item => !!item);
+    }
+    
+    const getColumn = (x) => {
+        let resultSet = [];
+        
+        fields.slice().map((row) => {
+            return resultSet.push(row[x] || null);
+        });
+        
+        return resultSet.filter(item => !!item);
+    }
+    
+    const getSection = (y,x) => {
+        let resultSet = [];
+
+        fields.slice(
+            Math.floor(y / section) * section,
+            section
+        ).map(
+            row => {
+                resultSet.push(
+                    ...row.slice(
+                        Math.floor(x / section) * section,
+                        section
+                    ).map(col => col?.value || 0)
+                );
+
+                return;
+            }
+        ).filter(item => !!item);
+        
+        return resultSet.filter(item => !!item);
+    }
 
     return (
         <div className="sudoku-board">
@@ -37,6 +90,8 @@ const Board = ({length}) => {
                     row={key} 
                     boardLength={length}
                     changeHandler={setField}
+                    activeRow={active[0]}
+                    activeColumn={active[1]}
                 />
             ))}
         </div>
--- src/components/row/index.js
+++ src/components/row/index.js
@@ -1,11 +1,9 @@
-import { useState, useEffect } from 'react';
 import './style.css';
 import Column from '../column';
 
-const Row = ({columns, row, changeHandler, boardLength}) => {
-    const sectionLength = Math.sqrt(boardLength.length);
+const Row = ({columns, row, changeHandler, boardLength, activeRow, activeColumn}) => {
+    const sectionLength = Math.sqrt(boardLength);
     const lastOfSection = (row + 1) % sectionLength===0;
-    const [cols, setCols] = useState(columns);
 
     const changeFieldHandler = (x, value) => {
         changeHandler(row, x, value);
@@ -13,12 +11,15 @@ const Row = ({columns, row, changeHandler, boardLength}) => {
 
     return (
         <div className={`row row-${row} ${lastOfSection ? 'row-section-last' : ''}`}>
-            {cols.map((col, key) => (
+            {columns.map((col, key) => (
                 <Column 
-                    value={col} 
+                    value={col?.value || 0} 
                     column={key} 
+                    row={row}
                     boardLength={boardLength}
                     changeHandler={changeFieldHandler} 
+                    isActive={activeRow===row && activeColumn===key}
+                    hasError={col?.error || false}
                 />
             ))}
         </div>
