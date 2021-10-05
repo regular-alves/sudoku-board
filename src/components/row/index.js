3e84503f42a8926df75bf2d4f11485b787e64a41
Rodrigo Alves <rodrigo.gois@live.com>
2021-10-04 22:18:10 -0300

    change: row handling error

--- src/components/row/index.js
+++ src/components/row/index.js
@@ -1,25 +1,27 @@
 import './style.css';
 import Column from '../column';
 
-const Row = ({columns, row, changeHandler, boardLength, activeRow, activeColumn}) => {
+const Row = ({columns, row, changeHandler, boardLength, activeRow, activeColumn, columnErrors}) => {
     const sectionLength = Math.sqrt(boardLength);
     const lastOfSection = (row + 1) % sectionLength===0;
+    const hasError = columns.filter(column => column?.error || false).length>1;
 
     const changeFieldHandler = (x, value) => {
         changeHandler(row, x, value);
     }
 
     return (
-        <div className={`row row-${row} ${lastOfSection ? 'row-section-last' : ''}`}>
+        <div className={`row row-${row} ${lastOfSection ? 'row-section-last' : ''} ${hasError ? 'error' : ''}`}>
             {columns.map((col, key) => (
                 <Column 
-                    value={col?.value || 0} 
+                    value={col?.value || ''} 
                     column={key} 
                     row={row}
                     boardLength={boardLength}
                     changeHandler={changeFieldHandler} 
                     isActive={activeRow===row && activeColumn===key}
                     hasError={col?.error || false}
+                    hasColumnError={columnErrors[key]>1}
                 />
             ))}
         </div>
