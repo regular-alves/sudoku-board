3dae836fcfdad5875eabd6b8dc3bc1c1fd4f1453
Rodrigo Alves <rodrigo.gois@live.com>
2021-10-14 21:56:54 -0300

    add: toogle style

--- src/components/toggle/index.js
+++ src/components/toggle/index.js
@@ -1,11 +1,13 @@
 import react from 'react';
+import './style.css';
 
 const Toogle = ({onChange, text}) => {
     return (
-        <div>
+        <div className="toogle-button">
             <span>{text}</span>
             <label>
                 <input type="checkbox" value="1" onChange={onChange} />
+                <span>&nbsp;</span>
             </label>
         </div>
     )
