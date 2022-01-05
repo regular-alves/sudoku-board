036fe2a08de4a6cdc6c39ba20849eaf9492cf4d2
Rodrigo Alves <rodrigo.gois@live.com>
2022-01-05 20:06:14 -0300

    set: page view

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from './App';
 import ReactGA from 'react-ga';
 
 ReactGA.initialize('UA-216506531-1');
+ReactGA.set({ page: '/' });
+ReactGA.pageview('/');
 
 ReactDOM.render(
   <React.StrictMode>
