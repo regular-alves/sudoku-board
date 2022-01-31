23a0df5c0236b2d99f3a9dfccd1b3c646b0129bd
Rodrigo Alves <rodrigo.gois@live.com>
2022-01-31 19:50:44 -0300

    fix: applying project standards on Column component

--- src/components/Column/index.js
+++ src/components/Column/index.js
@@ -1,43 +1,79 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './style.css';
 
-export default class Column extends React.Component {
-    constructor(props) {
-        super(props);
+class Column extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.sectionLength = Math.sqrt(this.props.boardLength);
-        this.lastOfSection = (this.props.column + 1) % this.sectionLength===0;
-    }
+    const {
+      column, boardLength, error, row, value, changeHandler, possible,
+    } = props;
 
-    render() {
-        return (
-            <div
-                className={
-                    `column column-${this.props.column} ` + 
-                    `section-${Math.floor((this.props.column + this.sectionLength) / this.sectionLength)} ` +
-                    `${this.lastOfSection ? 'section-last' : ''} ` +
-                    `${this.props.error ? 'error' : ''}` 
+    this.column = column;
+    this.error = error;
+    this.row = row;
+    this.value = value;
+    this.changeHandler = changeHandler;
+    this.possible = possible;
+
+    this.sectionLength = Math.sqrt(boardLength);
+    this.lastOfSection = (column + 1) % this.sectionLength === 0;
+  }
+
+  render() {
+    return (
+      <div
+        className={
+                    `column column-${this.column} `
+                    + `section-${Math.floor((this.column + this.sectionLength) / this.sectionLength)} `
+                    + `${this.lastOfSection ? 'section-last' : ''} `
+                    + `${this.error ? 'error' : ''}`
                 }
-            >
-                <input 
-                    id={`input-${this.props.row}-${this.props.column}`}
-                    type="number" 
-                    className="field"
-                    value={this.props.value}
-                    onChange={(e) => {
-                        this.props.changeHandler(this.props.column, e.target.value.substr(-1));
-                    }}
-                />
-                <div className="possibles">{
-                    this.props?.possible.length>0 
-                    && this.props.possible.map(i => (
-                        <label 
-                            htmlFor={`input-${this.props.row}-${this.props.column}`} 
-                            className="possible"
-                        >{i}</label>
-                    ))
-                }</div>
-            </div>
-        );
-    }
+      >
+        <input
+          id={`input-${this.row}-${this.column}`}
+          type="number"
+          className="field"
+          value={this.value}
+          onChange={(e) => {
+            this.changeHandler(this.column, e.target.value.substr(-1));
+          }}
+        />
+        <div className="possibles">
+          {
+            this.possible
+            && this.possible.length > 0
+            && this.possible.map((i) => (
+              <label
+                htmlFor={`input-${this.row}-${this.column}`}
+                className="possible"
+              >
+                {i}
+              </label>
+            ))
+          }
+        </div>
+      </div>
+    );
+  }
 }
+
+Column.defaultProps = {
+  error: false,
+  value: null,
+  changeHandler: () => {},
+  possible: [],
+};
+
+Column.propTypes = {
+  column: PropTypes.number.isRequired,
+  boardLength: PropTypes.number.isRequired,
+  error: PropTypes.bool,
+  row: PropTypes.number.isRequired,
+  value: PropTypes.number,
+  changeHandler: PropTypes.func,
+  possible: PropTypes.arrayOf(PropTypes.number),
+};
+
+export default Column;
