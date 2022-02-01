5eb88a65f85085268eb89d9eecc6f055de43381d
Rodrigo Alves <rodrigo.gois@live.com>
2022-01-31 21:06:29 -0300

    fix: applying project standards on Timer

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,34 +1,48 @@
-import {useState} from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 import './style.css';
 
-const Timer = ({ started, state = 0, setState }) => {
-    const [took, setTook] = useState(state===null || isNaN(state) ? 0 : parseInt(state));
-    const [startDate, setStartDate] = useState(0);
+function Timer(props) {
+  const { started, state, setState } = props;
+  const [took, setTook] = useState(state === null || Math.isNaN(state) ? 0 : parseInt(state, 10));
+  const [startDate, setStartDate] = useState(0);
 
-    if(started && !startDate) {
-        const time = new Date();
+  if (started && !startDate) {
+    const time = new Date();
 
-        time.setTime(time.getTime() + (-1 * took));
+    time.setTime(time.getTime() + (-1 * took));
 
-        setStartDate(new Date(time));
-    }
+    setStartDate(new Date(time));
+  }
 
-    if(started && startDate) {
-        setInterval(
-            () => {
-                setTook(new Date().getTime() - startDate.getTime());
-                setState(new Date().getTime() - startDate.getTime());
-            },
-            1000
-        );
-    }
-
-    return (
-        <div className="timer">
-            {new Date(took).toISOString().substr(11, 8)}
-        </div>
+  if (started && startDate) {
+    setInterval(
+      () => {
+        setTook(new Date().getTime() - startDate.getTime());
+        setState(new Date().getTime() - startDate.getTime());
+      },
+      1000,
     );
+  }
+
+  return (
+    <div className="timer">
+      {new Date(took).toISOString().substr(11, 8)}
+    </div>
+  );
 }
+
+Timer.defaultProps = {
+  started: false,
+  state: 0,
+  setState: () => {},
+};
+
+Timer.propTypes = {
+  started: PropTypes.bool,
+  state: PropTypes.number,
+  setState: PropTypes.func,
+};
 
 export default Timer;
