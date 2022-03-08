4b8406515c598f1de71750c1c04910104048f775
Rodrigo Alves <rodrigo.gois@live.com>
2022-03-07 21:01:12 -0300

    add: use of components on game

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import Board from '../Board';
+import OptionList from '../OptionList';
+import Timer from '../Timer';
 import './style.css';
 
 function Game() {
   return (
     <div className="content">
       <div className="game">
-        <div className="timer">14:48</div>
+        <Timer>14:48</Timer>
         <Board length={9} />
+        <OptionList
+          values={[
+            {
+              value: 1,
+              recommended: true,
+            }, {
+              value: 2,
+              recommended: true,
+            }, {
+              value: 3,
+              recommended: true,
+            }, {
+              value: 4,
+              recommended: true,
+            }, {
+              value: 5,
+              recommended: true,
+            }, {
+              value: 6,
+              recommended: true,
+            }, {
+              value: 7,
+              recommended: true,
+            }, {
+              value: 8,
+              recommended: true,
+            }, {
+              value: 9,
+              recommended: true,
+            },
+          ]}
+        />
       </div>
     </div>
   );
